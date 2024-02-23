992ce20e7669ff5d8668a29de97144eaa3cc1451
Timothy Dennis <timothyddennis@gmail.com>
2024-02-22 19:10:42 -0600

    feat: added quote module and .env file import to frontend of app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import InspoQuote from './sections/InspoQuote'
+import './stylesheets/App.css'
 
 function App() {
 
   return (
-    <>
+    <div className="container" id="root-container">
       <h1>Hello world!</h1>
-    </>
+      <div className="section-container" id="inspo-section-container">
+        <InspoQuote />
+      </div>
+    </div>
   )
 }
 
---
+++ client/src/sections/InspoQuote.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import "../stylesheets/inspoquote.css";
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_QUOTE_API_KEY;
+
+function InspoQuote() {
+	const [quote, setQuote] = useState({});
+
+	const getQuote = async () => {
+		let newQuote = await axios({
+			method: "GET",
+			url: `https://api.api-ninjas.com/v1/quotes?category=success`,
+			headers: {
+				"X-Api-Key": API_KEY,
+			},
+		});
+		console.log(newQuote.data);
+		setQuote(newQuote.data[0]);
+	};
+
+	useEffect(() => {
+		getQuote();
+	}, []);
+
+	return (
+		<div className='page-section' id='quote-section'>
+			{quote != {} ? (
+				<div id='quote-response'>
+					<p
+						id='quote-text'
+						className='quote-element quote-text'>
+						{quote.quote}
+					</p>
+					<h5
+						id='quote-author'
+						className='quote-element quote-author'>
+						{quote.author}
+					</h5>
+					<h6
+						id='quote-category'
+						className='quote-element quote-category'>
+						{quote.category}
+					</h6>
+				</div>
+			) : (
+				<div id='no-quote'></div>
+			)}
+		</div>
+	);
+}
+
+export default InspoQuote;
